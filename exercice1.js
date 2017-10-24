6fe18c558bb68b9e28f8973e21e8247911e0158b
AndreVictor60 <32367406+AndreVictor60@users.noreply.github.com>
2017-10-24 14:55:17 +0200

    Delete exercice1.js

--- exercice1.js
+++
@@ -1,6 +0,0 @@
-var image = document.getElementById('image1'),
-    result = document.getElementById('result');
-
-image.addEventListener('mouseover', function(e) {
-    result.innerHTML = "L'élément déclencheur de l'événement \"mouseover\" possède l'ID : " + e.target.id;
-});
